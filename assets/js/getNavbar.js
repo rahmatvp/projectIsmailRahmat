9537dbd2672634cb5e5dbab5a7a93ff684cbf1f1
b1994mi <b1994mi@gmail.com>
2021-01-06 13:57:43 +0700

    ubah href to origin

--- assets/js/getNavbar.js
+++ assets/js/getNavbar.js
@@ -3,6 +3,7 @@
     let rTxt = await fetch("assets/components/navbar.html").then(r => r.text())
     let navbar = parser.parseFromString(rTxt, 'text/html').querySelector(".navbar")
     $("body").append(navbar).css('padding-top', navbar.offsetHeight)
+    navbar.querySelector("a").href = window.location.origin
 })();
 
 $(window).resize(function () {
