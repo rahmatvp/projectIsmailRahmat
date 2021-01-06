a26c027a9d2b0344b3ea7f4e9bf8072e145149d0
b1994mi <b1994mi@gmail.com>
2021-01-06 14:02:20 +0700

    print semua property window.location

--- assets/js/getNavbar.js
+++ assets/js/getNavbar.js
@@ -4,6 +4,16 @@
     let navbar = parser.parseFromString(rTxt, 'text/html').querySelector(".navbar")
     $("body").append(navbar).css('padding-top', navbar.offsetHeight)
     navbar.querySelector("a").href = window.location.origin
+    console.log(window.location.origin)
+    console.log(window.location.href)
+    console.log(window.location.hash)
+    console.log(window.location.host)
+    console.log(window.location.hostname)
+    console.log(window.location.pathname)
+    console.log(window.location.port)
+    console.log(window.location.protocol)
+    console.log(window.location.search)
+    console.log(window.location.ancestorOrigins)
 })();
 
 $(window).resize(function () {
