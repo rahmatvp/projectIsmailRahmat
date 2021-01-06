de36c82601e5c2c2b893e6a18d13904e99da7c43
b1994mi <b1994mi@gmail.com>
2021-01-06 16:57:17 +0700

    breadcrumb di navbar sdh dinamis sesuai halamannya

--- assets/js/getNavbar.js
+++ assets/js/getNavbar.js
@@ -3,6 +3,34 @@
     let rTxt = await fetch("assets/components/navbar.html").then(r => r.text())
     let navbar = parser.parseFromString(rTxt, 'text/html').querySelector(".navbar")
     $("body").append(navbar).css('padding-top', navbar.offsetHeight)
+    let path = window.location.pathname
+    let ol = navbar.querySelector("ol")
+    switch (path.split("/")[1]) {
+        case "biodata.html":
+            ol.innerHTML +=
+                `<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item"><a href="klub.html?id=${kompId}&kompNama=${kompNama}">${kompNama}</a></li>
+                <li class="breadcrumb-item"><a href="pemain.html?id=${klubId}&klubNama=${klubNama}&kompId=${kompId}&kompNama=${kompNama}">${klubNama}</a></li>
+                <li class="breadcrumb-item active">${orgNama}</li>`
+            break;
+
+        case "pemain.html":
+            ol.innerHTML +=
+                `<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item"><a href="klub.html?id=${kompId}&kompNama=${kompNama}">${kompNama}</a></li>
+                <li class="breadcrumb-item active">${klubNama}</li>`
+            break;
+
+        case "klub.html":
+            ol.innerHTML +=
+                `<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item active">${kompNama}</li>`
+            break;
+
+        default:
+            ol.innerHTML += `<li class="breadcrumb-item active">Home</li>`
+            break;
+    }
 })();
 
 $(window).resize(function () {
